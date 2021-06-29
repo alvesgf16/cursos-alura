5a297f8f987e0553a42106dbc0a7f17bfed3b3d3
alvesgf16 <alvesgabriel1994@gmail.com>
2021-06-29 14:01:12 -0300

    Adicionando projetos feitos durante cursos da Alura

---
+++ javascript/projeto_inicial/componentes/deletaTarefa.js
@@ -0,0 +1,19 @@
+const BotaoDeleta = () => {
+	const botaoDeleta = document.createElement('button');
+	botaoDeleta.classList.add('');
+	botaoDeleta.innerText = 'DELETAR';
+
+	botaoDeleta.addEventListener('click', deletarTarefa);
+
+	return botaoDeleta;
+}
+
+const deletarTarefa = (evento) => {
+	const botaoDeleta = evento.target;
+
+	const tarefaDeletada = botaoDeleta.parentElement;
+
+	tarefaDeletada.remove();
+}
+
+export default BotaoDeleta
