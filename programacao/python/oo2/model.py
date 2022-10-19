fa833fdf03d9e519a1ec5eee402cb0bf05365b7f
alvesgf16 <alvesgabriel1994@gmail.com>
2022-10-19 10:28:08 -0300

    feat: implement parent class

--- programacao/python/oo2/model.py
+++ programacao/python/oo2/model.py
@@ -1,54 +1,42 @@
-class Film:
+class Media:
+    def __init__(self, name, year):
+        self._name = name.title()
+        self.year = year
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def like(self):
+        self._likes += 1
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name.title()
+
+
+class Film(Media):
     def __init__(self, name, year, length):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.length = length
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def like(self):
-        self.__likes += 1
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.title()
 
 
-class Series:
+class Series(Media):
     def __init__(self, name, year, seasons):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.seasons = seasons
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def like(self):
-        self.__likes += 1
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.title()
 
 
 avengers = Film('avengers: infinity war', 2018, 149)
 avengers.like()
-print(f'Name: {avengers.name} - Year: {avengers.year} - Length: {avengers.length} - Likes: {avengers.likes}')
+print(f'{avengers.name} - {avengers.year} - {avengers.length}: {avengers.likes}')
 
 atlanta = Series('atlanta', 2016, 4)
 atlanta.like()
 atlanta.like()
-print(f'Name: {atlanta.name} - Year: {atlanta.year} - Seasons: {atlanta.seasons} - Likes: {atlanta.likes}')
+print(f'{atlanta.name} - {atlanta.year} - {atlanta.seasons}: {atlanta.likes}')
