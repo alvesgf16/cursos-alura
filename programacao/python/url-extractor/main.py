e7bbb477b9ba5231f20a10a82d97008bd683c3a6
alvesgf16 <alvesgabriel1994@gmail.com>
2022-10-28 10:18:50 -0300

    chore: update url and add comments

--- programacao/python/url-extractor/main.py
+++ programacao/python/url-extractor/main.py
@@ -1,5 +1,6 @@
 url = (
-    "bytebank.com/cambio?moedaOrigem=real"
+    "https://bytebank.com/cambio?"
+    + "quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 )
 
 question_mark_index = url.find("?")
@@ -7,9 +8,11 @@ question_mark_index = url.find("?")
 if question_mark_index == -1:
     base_url = url
 else:
+    # Separate base and parameters
     base_url = url[:question_mark_index]
-    url_parameters = url[question_mark_index + 1:36]
+    url_parameters = url[question_mark_index + 1:]
 
+    # Look for the value of a parameter
     search_parameter = "moedaOrigem"
     parameter_index = url_parameters.find(search_parameter)
     value_index = parameter_index + len(search_parameter) + 1
