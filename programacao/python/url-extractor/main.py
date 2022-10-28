6ad3fd80c81b506b63a30a3b8eb41c1fcb549955
alvesgf16 <alvesgabriel1994@gmail.com>
2022-10-28 10:16:08 -0300

    feat: check if url has parameters

--- programacao/python/url-extractor/main.py
+++ programacao/python/url-extractor/main.py
@@ -2,5 +2,10 @@ url = (
     "bytebank.com/cambio?moedaOrigem=real"
 )
 
-base_url = url[:19]
-url_parameters = url[20:36]
+question_mark_index = url.find("?")
+
+if question_mark_index == -1:
+    base_url = url
+else:
+    base_url = url[:question_mark_index]
+    url_parameters = url[question_mark_index + 1:36]
