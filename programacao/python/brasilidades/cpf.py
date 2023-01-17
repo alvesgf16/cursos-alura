60ef872e7714e95fa021345188e5676f1dfc00ec
alvesgf16 <alvesgabriel1994@gmail.com>
2023-01-17 15:46:45 -0300

    refactor: swap if/else branches in constructor

--- programacao/python/brasilidades/cpf.py
+++ programacao/python/brasilidades/cpf.py
@@ -4,10 +4,10 @@ from validate_docbr import CPF
 class Cpf:
     def __init__(self, document):
         document = str(document)
-        if self.is_cpf_valid(document):
-            self.cpf = document
-        else:
+        if not self.is_cpf_valid(document):
             raise ValueError("Invalid CPF!")
+        else:
+            self.cpf = document
 
     def __str__(self):
         return self.format_cpf()
