bfc60b028509fc214e3ccfcce89e9e31666e5a8a
alvesgf16 <alvesgabriel1994@gmail.com>
2023-01-17 14:26:46 -0300

    feat: display cpf with formatting

--- programacao/python/brasilidades/cpf.py
+++ programacao/python/brasilidades/cpf.py
@@ -6,9 +6,18 @@ class Cpf:
         else:
             raise ValueError("Invalid CPF!")
 
+    def __str__(self):
+        return self.format_cpf()
+
     def is_cpf_valid(self, document):
         if len(document) == 11:
             return True
         else:
             return False
 
+    def format_cpf(self):
+        slice_one = self.cpf[:3]
+        slice_two = self.cpf[3:6]
+        slice_three = self.cpf[6:9]
+        slice_four = self.cpf[9:]
+        return f"{slice_one}.{slice_two}.{slice_three}-{slice_four}"
--- programacao/python/brasilidades/test_cpf.py
+++ programacao/python/brasilidades/test_cpf.py
@@ -2,6 +2,11 @@ import pytest
 from cpf import Cpf
 
 
+def test_valid_cpf_is_displayed_with_formatting():
+    cpf = Cpf(15616987913)
+    assert str(cpf) == "156.169.879-13"
+
+
 def test_cpf_with_length_other_than_11_raises_an_exception():
     with pytest.raises(ValueError, match="Invalid CPF"):
         Cpf(1561698791)
