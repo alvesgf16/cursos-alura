bf63710da46f2c5a274312264dd2a3e026bb1f9c
alvesgf16 <alvesgabriel1994@gmail.com>
2023-01-18 11:59:10 -0300

    feat: display phone number with formatting

--- programacao/python/brasilidades/telephone.py
+++ programacao/python/brasilidades/telephone.py
@@ -3,15 +3,27 @@ import re
 
 class Telephone:
     def __init__(self, phone_number):
+        self.pattern = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
         if self.validate(phone_number):
             self.number = phone_number
         else:
             raise ValueError("Invalid number!")
 
+    def __str__(self):
+        return self.format()
+
     def validate(self, phone_number):
-        pattern = "[0-9]{2,3}[0-9]{2}[0-9]{4,5}[0-9]{4}"
-        result = re.findall(pattern, phone_number)
+        result = re.findall(self.pattern, phone_number)
         if result:
             return True
         else:
             return False
+
+    def format(self):
+        result = re.search(self.pattern, self.number)
+        return "+{} ({}) {}-{}".format(
+            result.group(1),
+            result.group(2),
+            result.group(3),
+            result.group(4)
+        )
--- programacao/python/brasilidades/test_telephone.py
+++ programacao/python/brasilidades/test_telephone.py
@@ -6,3 +6,7 @@ def test_invalid_phone_number_raises_an_error():
     with pytest.raises(ValueError, match="Invalid number!"):
         Telephone("26481234")
 
+
+def test_valid_phone_number_is_displayed_with_formatting():
+    phone = Telephone("552126481234")
+    assert str(phone) == "+55 (21) 2648-1234"
