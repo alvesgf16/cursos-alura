34b40bb2b304aca64a4a6651e4db1e72abc76d40
alvesgf16 <alvesgabriel1994@gmail.com>
2023-01-18 12:58:44 -0300

    refactor: replace temp number parts with query

--- programacao/python/brasilidades/telephone.py
+++ programacao/python/brasilidades/telephone.py
@@ -12,10 +12,13 @@ class Telephone:
     def __str__(self):
         return self.format()
 
+    @property
+    def number_parts(self):
+        return re.search(self.pattern, self.number).groups()
+
     def is_valid(self, phone_number):
         return bool(re.findall(self.pattern, phone_number))
 
     def format(self):
-        phone_number_parts = re.search(self.pattern, self.number).groups()
-        country_code, area_code, prefix, line_number = phone_number_parts
+        country_code, area_code, prefix, line_number = self.number_parts
         return f"+{country_code} ({area_code}) {prefix}-{line_number}"
