1ac596fd6490329edd8636e2fb199b399b029bb2
alvesgf16 <alvesgabriel1994@gmail.com>
2023-01-20 11:01:15 -0300

    initial commit of tdd course project

---
+++ programacao/python/tdd/src/bytebank.py
@@ -0,0 +1,31 @@
+from datetime import date
+
+
+class Employee:
+    def __init__(self, name, birth_date, salary):
+        self._name = name
+        self._birth_date = birth_date
+        self._salary = salary
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def salary(self):
+        return self._salary
+
+    def age(self):
+        split_birth_date = self._birth_date.split("/")
+        birth_year = split_birth_date[-1]
+        current_year = date.today().year
+        return current_year - int(birth_year)
+
+    def calcularte_bonus(self):
+        value = self._salary * 0.1
+        if value > 1000:
+            value = 0
+        return value
+
+    def __str__(self):
+        return f"Funcionario({self._name}, {self._birth_date}, {self._salary})"
