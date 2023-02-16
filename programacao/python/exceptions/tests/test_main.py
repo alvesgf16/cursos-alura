51207085b2b45016f29ef9f4d2d14ecb1fe633af
alvesgf16 <alvesgabriel1994@gmail.com>
2023-02-16 14:52:37 -0300

    test: add xfail test for non-existent attribute

--- programacao/python/exceptions/tests/test_main.py
+++ programacao/python/exceptions/tests/test_main.py
@@ -1,4 +1,5 @@
 from main import Customer
+import pytest
 
 
 def test_customer_has_a_name_cpf_and_occupation():
@@ -11,3 +12,15 @@ def test_customer_has_a_name_cpf_and_occupation():
     assert customer.name == name
     assert customer.cpf == cpf
     assert customer.occupation == occupation
+
+
+@pytest.mark.xfail
+def test_customer_has_an_age():
+    name = "a name"
+    cpf = "123.456.789-01"
+    occupation = "student"
+    age = 18
+
+    customer = Customer(name, cpf, occupation)
+
+    assert customer.age == age
