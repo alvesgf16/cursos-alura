b0fa6557aa6a9b67f9e6311e3a84ba9b2d946ea6
alvesgf16 <alvesgabriel1994@gmail.com>
2023-02-16 16:16:50 -0300

    refactor(test): move account init to setup

--- programacao/python/exceptions/tests/test_main.py
+++ programacao/python/exceptions/tests/test_main.py
@@ -11,6 +11,9 @@ class TestMain(unittest.TestCase):
         self.cpf = "123.456.789-01"
         self.occupation = "student"
         self.customer = Customer(self.name, self.cpf, self.occupation)
+        self.agency = "1"
+        self.number = "00001"
+        self.account = CurrentAccount(self.customer, self.agency, self.number)
 
     def test_new_customer_has_a_name_cpf_and_occupation(self):
         assert self.customer.name == self.name
@@ -24,27 +27,12 @@ class TestMain(unittest.TestCase):
         assert self.customer.age == age
 
     def test_new_current_account_has_a_customer_agency_and_number(self):
-        agency = "1"
-        number = "00001"
-
-        account = CurrentAccount(self.customer, agency, number)
-
-        assert account.customer == self.customer
-        assert account.agency == agency
-        assert account.number == number
+        assert self.account.customer == self.customer
+        assert self.account.agency == self.agency
+        assert self.account.number == self.number
 
     def test_new_account_is_initialized_with_a_balance_of_100(self):
-        agency = "1"
-        number = "00001"
-
-        account = CurrentAccount(self.customer, agency, number)
-
-        assert account.balance == 100
+        assert self.account.balance == 100
 
     def test_adding_an_account_increases_the_total_accounts_created(self):
-        agency = "1"
-        number = "00001"
-
-        CurrentAccount(self.customer, agency, number)
-
         assert CurrentAccount.total_accounts_created == 1
