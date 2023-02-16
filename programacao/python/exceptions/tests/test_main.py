9aa2371fc358365ccf83604f9f873648d6b95755
alvesgf16 <alvesgabriel1994@gmail.com>
2023-02-16 15:55:48 -0300

    refactor(test): combine test functions into class
    Use unittest to create a test case class. In the process of importing
    unittest, also organize imports.

--- programacao/python/exceptions/tests/test_main.py
+++ programacao/python/exceptions/tests/test_main.py
@@ -1,42 +1,44 @@
-from main import Customer, CurrentAccount
+import unittest
+
 import pytest
 
-
-def test_customer_has_a_name_cpf_and_occupation():
-    name = "a name"
-    cpf = "123.456.789-01"
-    occupation = "student"
-    
-    customer = Customer(name, cpf, occupation)
-
-    assert customer.name == name
-    assert customer.cpf == cpf
-    assert customer.occupation == occupation
+from main import Customer, CurrentAccount
 
 
-@pytest.mark.xfail
-def test_customer_has_an_age():
-    name = "a name"
-    cpf = "123.456.789-01"
-    occupation = "student"
-    age = 18
+class TestMain(unittest.TestCase):
+    def test_customer_has_a_name_cpf_and_occupation(self):
+        name = "a name"
+        cpf = "123.456.789-01"
+        occupation = "student"
 
-    customer = Customer(name, cpf, occupation)
+        customer = Customer(name, cpf, occupation)
 
-    assert customer.age == age
+        assert customer.name == name
+        assert customer.cpf == cpf
+        assert customer.occupation == occupation
 
+    @pytest.mark.xfail
+    def test_customer_has_an_age(self):
+        name = "a name"
+        cpf = "123.456.789-01"
+        occupation = "student"
+        age = 18
 
-def test_current_account_has_a_customer_agency_and_number():
-    name = "a name"
-    cpf = "123.456.789-01"
-    occupation = "student"
-    agency = "1"
-    number = "00001"
+        customer = Customer(name, cpf, occupation)
 
-    customer = Customer(name, cpf, occupation)
+        assert customer.age == age
 
-    account = CurrentAccount(customer, agency, number)
+    def test_current_account_has_a_customer_agency_and_number(self):
+        name = "a name"
+        cpf = "123.456.789-01"
+        occupation = "student"
+        agency = "1"
+        number = "00001"
 
-    assert account.customer == customer
-    assert account.agency == agency
-    assert account.number == number
+        customer = Customer(name, cpf, occupation)
+
+        account = CurrentAccount(customer, agency, number)
+
+        assert account.customer == customer
+        assert account.agency == agency
+        assert account.number == number
