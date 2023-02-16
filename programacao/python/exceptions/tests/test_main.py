c6c1507e9f0db2354f3c07228805cabab329af75
alvesgf16 <alvesgabriel1994@gmail.com>
2023-02-16 15:56:02 -0300

    refactor(test): extract setup method

--- programacao/python/exceptions/tests/test_main.py
+++ programacao/python/exceptions/tests/test_main.py
@@ -6,39 +6,29 @@ from main import Customer, CurrentAccount
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.name = "a name"
+        self.cpf = "123.456.789-01"
+        self.occupation = "student"
+        self.customer = Customer(self.name, self.cpf, self.occupation)
+
     def test_customer_has_a_name_cpf_and_occupation(self):
-        name = "a name"
-        cpf = "123.456.789-01"
-        occupation = "student"
-
-        customer = Customer(name, cpf, occupation)
-
-        assert customer.name == name
-        assert customer.cpf == cpf
-        assert customer.occupation == occupation
+        assert self.customer.name == self.name
+        assert self.customer.cpf == self.cpf
+        assert self.customer.occupation == self.occupation
 
     @pytest.mark.xfail
     def test_customer_has_an_age(self):
-        name = "a name"
-        cpf = "123.456.789-01"
-        occupation = "student"
         age = 18
 
-        customer = Customer(name, cpf, occupation)
-
-        assert customer.age == age
+        assert self.customer.age == age
 
     def test_current_account_has_a_customer_agency_and_number(self):
-        name = "a name"
-        cpf = "123.456.789-01"
-        occupation = "student"
         agency = "1"
         number = "00001"
 
-        customer = Customer(name, cpf, occupation)
+        account = CurrentAccount(self.customer, agency, number)
 
-        account = CurrentAccount(customer, agency, number)
-
-        assert account.customer == customer
+        assert account.customer == self.customer
         assert account.agency == agency
         assert account.number == number
